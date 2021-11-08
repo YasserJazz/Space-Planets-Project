e1b157d7176b778b8ba61fbe88b77e05a65c4e54
Muhammad Yasser Jaziralhy <muhmmad.yas.jaz@gmail.com>
2021-11-08 16:32:04 +0200

    adding_more_habitability_conditions

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const parse = require("csv-parse");
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
-  return planet["koi_disposition"] === "CONFIRMED";
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 && //light
+    planet["koi_insol"] < 1.11 && //light
+    planet["koi_prad"] < 1.6 //radius
+  );
 }
 
 fs.createReadStream("./kepler_data.csv")
@@ -21,5 +26,5 @@ fs.createReadStream("./kepler_data.csv")
     console.log(err);
   })
   .on("end", () => {
-    console.log(habitablePlanets);
+    console.log(`${habitablePlanets.length} habitable planet found!`);
   });
