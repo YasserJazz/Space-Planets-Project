bc10ab90fe024ce070ab8fd6f2df6e4693f64a1a
Muhammad Yasser Jaziralhy <muhmmad.yas.jaz@gmail.com>
2021-11-08 13:23:16 +0200

    adding_fs_read_stream

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const parse = require('csv-parse');
+const fs = require("fs");
+const parse = require("csv-parse");
+
+const result = [];
+
+fs.createReadStream("./kepler_data.csv")
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(result);
+  });
